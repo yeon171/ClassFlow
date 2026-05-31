837996fff8e1bd8737cd35f16416eaaec47f4cf8
yeon171 <yeondy01@naver.com>
2026-05-31 18:09:15 +0900

    "Initial commit"

---
+++ src/main/java/com/example/classflow/ClassFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.classflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/classflow/ClassFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
